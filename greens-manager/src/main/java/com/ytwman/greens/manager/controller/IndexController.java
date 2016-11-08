427fa609d47853305a24d0182abbfe47b048cf9e
huhu <huhu@xiaokakeji.com>
2016-11-09 00:23:55 +0800

    添加文件

--- greens-manager/src/main/java/com/ytwman/greens/manager/controller/IndexController.java
+++ greens-manager/src/main/java/com/ytwman/greens/manager/controller/IndexController.java
@@ -7,9 +7,6 @@
 package com.ytwman.greens.manager.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author 忽忽(huhu)
@@ -19,9 +16,4 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class IndexController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/")
-    public Object index(ModelAndView modelAndView) {
-        modelAndView.setViewName("index");
-        return modelAndView;
-    }
 }
