7172f26cb96899f21cd0e0aee1ead133bfaa52e3
huhu <huhu@xiaokakeji.com>
2016-12-17 01:21:52 +0800

    文件提交

---
+++ greens-commons/src/main/java/com/ytwman/greens/commons/core/exception/ValueAbsentException.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C), 2014-2015, 杭州小卡科技有限公司
+ * Author:  忽忽(huhu)
+ * Date:    16/12/17 上午1:04
+ * Description: 
+ */
+package com.ytwman.greens.commons.core.exception;
+
+/**
+ * @author 忽忽(huhu)
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+public class ValueAbsentException extends Throwable {
+    public ValueAbsentException() {
+        super();
+    }
+
+    public ValueAbsentException(String message) {
+        super(message);
+    }
+}
