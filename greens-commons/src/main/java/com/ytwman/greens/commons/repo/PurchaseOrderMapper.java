74a9bed7547a07384f7ae0e324490c6b04afe34f
huhu <huhu@xiaokakeji.com>
2016-12-26 16:05:22 +0800

    采购单逻辑

--- greens-commons/src/main/java/com/ytwman/greens/commons/repo/PurchaseOrderMapper.java
+++ greens-commons/src/main/java/com/ytwman/greens/commons/repo/PurchaseOrderMapper.java
@@ -1,7 +1,33 @@
 package com.ytwman.greens.commons.repo;
 
+import com.ytwman.greens.commons.entity.PurchaseOrderEntity;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by ytwman on 16/12/23.
  */
-public class PurchaseOrderMapper {
+public interface PurchaseOrderMapper {
+
+    /**
+     * 根据主键查询
+     *
+     * @param id
+     * @return
+     */
+    PurchaseOrderEntity findById(@Param("id") Long id);
+
+    /**
+     * 根据关键字模糊查询
+     *
+     * @param keywords      采购单号
+     * @param purchaserId   采购人
+     * @param purchaserDate 采购时间
+     * @return
+     */
+    List<PurchaseOrderEntity> findAll(@Param("keywords") String keywords,
+                                      @Param("purchaserId") Long purchaserId,
+                                      @Param("purchaserDate") Date purchaserDate);
 }
--- greens-html5/assets/javascripts/goods/purchase/add_purchase.js
+++ greens-html5/assets/javascripts/goods/purchase/add_purchase.js
@@ -8,7 +8,7 @@ $(function () {
         border: false, // 是否显示边框
         rownumbers: true, // 是否显示索引
         // fit: true, // 填充父容器
-        height: '224',
+        height: '345',
         striped: true, // 是否开启行颜色间隔
         singleSelect: true, // 是否开启单行选择
         selectOnCheck: true, // 是否开启选择后选中行
@@ -17,6 +17,51 @@ $(function () {
         fitColumns: true, // 自动设置表格宽度，方式横向滚动
         toolbar: '#purchase-order-toolbar',
         url: projectPath + '/purchase',
-        method: 'get'
+        method: 'get',
+        // 双击编辑行记录
+        onClickCell: onDblClickCell,
+        onEndEdit: onEndEdit
     });
+
+    // 编辑行索引
+    var editIndex = undefined;
+
+    function endEditing() {
+        if (editIndex == undefined) {
+            return true
+        }
+        if ($('#dg').datagrid('validateRow', editIndex)) {
+            $('#dg').datagrid('endEdit', editIndex);
+            editIndex = undefined;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function onDblClickCell(index, field) {
+        if (editIndex != index) {
+            if (endEditing()) {
+                $('#dg').datagrid('selectRow', index)
+                    .datagrid('beginEdit', index);
+                var ed = $('#dg').datagrid('getEditor', {index: index, field: field});
+                if (ed) {
+                    ($(ed.target).data('textbox') ? $(ed.target).textbox('textbox') : $(ed.target)).focus();
+                }
+                editIndex = index;
+            } else {
+                setTimeout(function () {
+                    $('#dg').datagrid('selectRow', editIndex);
+                }, 0);
+            }
+        }
+    }
+
+    function onEndEdit(index, row) {
+        var ed = $(this).datagrid('getEditor', {
+            index: index,
+            field: 'productid'
+        });
+        row.productname = $(ed.target).combobox('getText');
+    }
 });
