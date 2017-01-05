4998b5c0bdf45985084a70d72c817af747edf459
huhu <huhu@xiaokakeji.com>
2017-01-05 13:58:31 +0800

    参数格式化

--- greens-commons/src/main/java/com/ytwman/greens/commons/core/web/StringToDateConverter.java
+++ greens-commons/src/main/java/com/ytwman/greens/commons/core/web/StringToDateConverter.java
@@ -1,7 +1,10 @@
 package com.ytwman.greens.commons.core.web;
 
+import com.ytwman.greens.commons.helper.DateFormatter;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.convert.converter.Converter;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -11,6 +14,22 @@ public class StringToDateConverter implements Converter<String, Date> {
 
     @Override
     public Date convert(String source) {
-        return source == null || source.isEmpty() ? null : new Date(Long.valueOf(source));
+        Date date = null;
+
+        if (StringUtils.isEmpty(source)) {
+            return null;
+        }
+
+        try {
+            date = new SimpleDateFormat(DateFormatter.FORMAT_DATETIME).parse(source);
+        } catch (Exception e) {
+            try {
+                date = new SimpleDateFormat(DateFormatter.FORMAT_DATE).parse(source);
+            } catch (Exception e1) {
+                date = new Date(Long.valueOf(source));
+            }
+        }
+
+        return date;
     }
 }
